ae2380c3aadb884476f98bbe75de3ebd66cf93ab
Marek Eibel <536376@mail.muni.cz>
2025-03-25 20:52:48 +0100

    fix: add intermediate fucnton for string to export format conversion

--- to_geom/src/core/config/ToGeomConfig.hpp
+++ to_geom/src/core/config/ToGeomConfig.hpp
@@ -2,49 +2,77 @@
 
 #include <filesystem>
 #include <memory>
+#include <string>
 
 #include <result.hpp>
 
 #include <nlohmann/json.hpp>
 
+#include "Config.hpp"
 #include "Error.hpp"
 #include "ExportFormats.hpp"
+#include "FormatString.hpp"
 #include "JsonError.hpp"
 #include "JsonFileReader.hpp"
 #include "Logger.hpp"
 #include "VrmlProcConfig.hpp"
-#include <FormatString.hpp>
 
 namespace to_geom::core::config {
 
-  inline to_geom::core::io::ExportFormat StringToExportFormat(const std::string& str) {
-    if (str == "stl" || str == "STL") return to_geom::core::io::ExportFormat::Stl;
-    if (str == "ply" || str == "PLY") return to_geom::core::io::ExportFormat::Ply;
-    if (str == "obj" || str == "OBJ") return to_geom::core::io::ExportFormat::Obj;
+  /**
+   * @brief Helper fucntion which converts string to ExportFormat and logs the process.
+   *
+   * @param string string to convert
+   * @returns ExportFormat after conversion
+   */
+  inline to_geom::core::io::ExportFormat ConvertStringToExportFormat(const std::string& string) {
+    using namespace to_geom::core::io;
+    bool defaultUsed;
+    auto format = ExportFormatUtils::StringToExportFormat(string, ExportFormat::Stl, defaultUsed);
 
-    vrml_proc::core::logger::LogWarning(vrml_proc::core::utils::FormatString(
-                                            "While parsing JSON field <exportFormat>, an error occured. The value <",
-                                            str, "> is not an expected value! The default value (<stl>) will be used."),
-                                        LOGGING_INFO);
+    if (defaultUsed) {
+      vrml_proc::core::logger::LogWarning(
+          vrml_proc::core::utils::FormatString("While parsing JSON field <exportFormat>, an error occured. The value <",
+              string, "> is not an expected value! The default value (<stl>) will be used."),
+          LOGGING_INFO);
+    }
 
-    return to_geom::core::io::ExportFormat::Stl;
+    return format;
   }
 
-  struct ToGeomConfig {
+  /**
+   * @brief Represents a configuration file for `togeom` library.
+   *
+   * Configuration file for `togeom` has following properties:
+   *  - exportFormat (ExportFormat),
+   *  - vrmlProcConfig (inherited properties from VrmlProcConfig class).
+   *
+   * @implements `Config` class with Load() method.
+   */
+  struct ToGeomConfig : public vrml_proc::core::config::Config {
+    /**
+     * @brief Defaul constructor.
+     */
     ToGeomConfig() = default;
 
     vrml_proc::core::config::VrmlProcConfig vrmlProcConfig;
     to_geom::core::io::ExportFormat exportFormat = to_geom::core::io::ExportFormat::Stl;
 
-    cpp::result<void, std::shared_ptr<vrml_proc::core::error::Error>> LoadFromJsonFile(
-        const std::filesystem::path& filepath) {
+    /**
+     * @brief Loads configuration file from JSON file.
+     *
+     * @param filepath path to the file to load
+     * @return result containing error on failure, else empty
+     */
+    cpp::result<void, std::shared_ptr<vrml_proc::core::error::Error>> Load(
+        const std::filesystem::path& filepath) override {
       vrml_proc::core::io::JsonFileReader reader;
       auto json = reader.Read(filepath);
 
       if (json.has_value()) {
         try {
           vrmlProcConfig.ignoreUnknownNode = json.value().value("ignoreUnknownNode", false);
-          exportFormat = StringToExportFormat(json.value().value("exportFormat", "stl"));
+          exportFormat = ConvertStringToExportFormat(json.value().value("exportFormat", "stl"));
           vrmlProcConfig.logFileDirectory =
               json.value().value("logFileDirectory", std::filesystem::current_path().string());
           vrmlProcConfig.logFileName = json.value().value("logFileName", "vrmlproc");
