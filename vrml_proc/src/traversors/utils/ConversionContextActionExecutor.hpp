a3ca43a49eddb2a8c6d8e90fb3fe638520a26ad2
Marek Eibel <536376@mail.muni.cz>
2025-04-07 14:20:19 +0200

    feat: add TryToExecute overload for HandlerToActionBundle object

--- vrml_proc/src/traversors/utils/ConversionContextActionExecutor.hpp
+++ vrml_proc/src/traversors/utils/ConversionContextActionExecutor.hpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 
+#include "NodeView.hpp"
+#include "HandlerToActionBundle.hpp"
+
 #include "ConversionContextActionMap.hpp"
 
 namespace vrml_proc {
@@ -16,10 +19,41 @@ namespace vrml_proc {
         template <typename ConversionContextType>
         static std::shared_ptr<ConversionContextType> TryToExecute(
             const vrml_proc::action::ConversionContextActionMap<ConversionContextType>& actionMap,
-            const std::string& key, const std::vector<std::reference_wrapper<const std::any>>& refArgs,
+            const std::string& key,
+            const std::vector<std::reference_wrapper<const std::any>>& refArgs,
             const std::vector<std::any>& copyArgs) {
           if (actionMap.VerifyKey(key)) {
             std::shared_ptr<ConversionContextType> result = actionMap.GetAction(key, refArgs, copyArgs)->Execute();
+            auto derivedContext = std::dynamic_pointer_cast<ConversionContextType>(result);
+            if (derivedContext != nullptr) {
+              return derivedContext;
+            }
+          }
+          return std::make_shared<ConversionContextType>();
+        }
+
+        /*template <typename ConversionContextType>
+        static std::shared_ptr<ConversionContextType> TryToExecute(
+            const vrml_proc::action::ConversionContextActionMap<ConversionContextType>& actionMap,
+            const std::string& key,
+            std::shared_ptr<vrml_proc::traversor::node_descriptor::NodeView> nodeView) {
+          if (actionMap.VerifyKeyForNodeView(key)) {
+            std::shared_ptr<ConversionContextType> result = actionMap.GetAction(key, nodeView)->Execute();
+            auto derivedContext = std::dynamic_pointer_cast<ConversionContextType>(result);
+            if (derivedContext != nullptr) {
+              return derivedContext;
+            }
+          }
+          return std::make_shared<ConversionContextType>();
+        }*/
+
+        template <typename ConversionContextType>
+        static std::shared_ptr<ConversionContextType> TryToExecute(
+            const vrml_proc::action::ConversionContextActionMap<ConversionContextType>& actionMap,
+            const std::string& key,
+            vrml_proc::traversor::handler::HandlerToActionBundle<ConversionContextType> data) {
+          if (actionMap.VerifyKeyForHandlerToActionBundle(key)) {
+            std::shared_ptr<ConversionContextType> result = actionMap.GetAction(key, data)->Execute();
             auto derivedContext = std::dynamic_pointer_cast<ConversionContextType>(result);
             if (derivedContext != nullptr) {
               return derivedContext;
