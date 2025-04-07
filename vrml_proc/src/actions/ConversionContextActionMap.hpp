03078630df0c4ea4f9608f9c3eae0efbc8a33c8d
Marek Eibel <536376@mail.muni.cz>
2025-04-07 14:22:48 +0200

    feat: tmp - ccactionmap needs additional methods to store handler to action bundle
    TODO: this will be cleaned, basically we can get rid of these Copied and Referenced Arguments

--- vrml_proc/src/actions/ConversionContextActionMap.hpp
+++ vrml_proc/src/actions/ConversionContextActionMap.hpp
@@ -8,6 +8,8 @@
 #include "ConversionContextAction.hpp"
 #include "Logger.hpp"
 #include "FormatString.hpp"
+#include "NodeView.hpp"
+#include "HandlerToActionBundle.hpp"
 
 #include "VrmlProcessingExport.hpp"
 
@@ -29,12 +31,25 @@ namespace vrml_proc {
        * These argument will be copied.
        */
       using CopiedArguments = std::vector<std::any>;
+
+      /**
+       * @brief Argument represented by a NodeView shared pointer.
+       */
+      using NodeViewArgument = std::shared_ptr<vrml_proc::traversor::node_descriptor::NodeView>;
+
+      using HandlerToActionBundleArgument = vrml_proc::traversor::handler::HandlerToActionBundle<ConversionContext>;
       /**
        * @brief The basic data type stored in the ActionMap. It is a function pointer to a function, which
        * takes some, for now unknown, arguments and returns a shared pointer owning ConversionContextAction.
        */
       using ActionFunctor = std::function<std::shared_ptr<ConversionContextAction<ConversionContext>>(
           ReferencedArguments, CopiedArguments)>;
+
+      using NodeViewActionFunctor =
+          std::function<std::shared_ptr<ConversionContextAction<ConversionContext>>(NodeViewArgument)>;
+
+      using HandlerToActionBundleActionFunctor =
+          std::function<std::shared_ptr<ConversionContextAction<ConversionContext>>(HandlerToActionBundleArgument)>;
       /**
        * @brief Adds new action to the ActionMap mapped to a string key.
        * Duplicated key will be rewritten.
@@ -43,10 +58,23 @@ namespace vrml_proc {
        * @param action ActionFunctor representing the given Action
        */
       void AddAction(const std::string& key, ActionFunctor action) {
-        vrml_proc::core::logger::LogDebug(vrml_proc::core::utils::FormatString("Add new action with key <", key, ">."),
-                                          LOGGING_INFO);
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Add new action with key <", key, ">."), LOGGING_INFO);
         m_actions[key] = std::move(action);
       }
+
+      void AddAction(const std::string& key, NodeViewActionFunctor action) {
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Add new action with key <", key, ">."), LOGGING_INFO);
+        m_nodeViewActions[key] = std::move(action);
+      }
+
+      void AddActionForHandlerToActionBundle(const std::string& key, HandlerToActionBundleActionFunctor action) {
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Add new action with key <", key, ">."), LOGGING_INFO);
+        m_handlerToActionBundleActions[key] = std::move(action);
+      }
+
       /**
        * @brief Verifies if the given key exists in the ActionMap.
        *
@@ -55,14 +83,37 @@ namespace vrml_proc {
        */
       bool VerifyKey(const std::string& key) const {
         bool result = (m_actions.find(key) != m_actions.end());
-        vrml_proc::core::logger::LogTrace(vrml_proc::core::utils::FormatString("Verify key <", key, ">."),
-                                          LOGGING_INFO);
+        vrml_proc::core::logger::LogTrace(
+            vrml_proc::core::utils::FormatString("Verify key <", key, ">."), LOGGING_INFO);
         if (result) {
-          vrml_proc::core::logger::LogTrace(vrml_proc::core::utils::FormatString("Key <", key, "> was not found!"),
-                                            LOGGING_INFO);
+          vrml_proc::core::logger::LogTrace(
+              vrml_proc::core::utils::FormatString("Key <", key, "> was not found!"), LOGGING_INFO);
         }
         return result;
       }
+
+      bool VerifyKeyForNodeView(const std::string& key) const {
+        bool result = (m_nodeViewActions.find(key) != m_nodeViewActions.end());
+        vrml_proc::core::logger::LogTrace(
+            vrml_proc::core::utils::FormatString("Verify key <", key, ">."), LOGGING_INFO);
+        if (result) {
+          vrml_proc::core::logger::LogTrace(
+              vrml_proc::core::utils::FormatString("Key <", key, "> was not found!"), LOGGING_INFO);
+        }
+        return result;
+      }
+
+      bool VerifyKeyForHandlerToActionBundle(const std::string& key) const {
+        bool result = (m_handlerToActionBundleActions.find(key) != m_handlerToActionBundleActions.end());
+        vrml_proc::core::logger::LogTrace(
+            vrml_proc::core::utils::FormatString("Verify key <", key, ">."), LOGGING_INFO);
+        if (result) {
+          vrml_proc::core::logger::LogTrace(
+              vrml_proc::core::utils::FormatString("Key <", key, "> was not found!"), LOGGING_INFO);
+        }
+        return result;
+      }
+
       /**
        * @brief Gets a shared pointer owning a ConversionContextAction object. The function creates new object using
        * functor stored in the ActionMap and passed `args` which are passed in to the Action constructor.
@@ -72,11 +123,10 @@ namespace vrml_proc {
        * @returns nullptr if the key does not exists, otherwise shared pointer owning the given ConversionContextAction
        * object
        */
-      std::shared_ptr<ConversionContextAction<ConversionContext>> GetAction(const std::string& key,
-                                                                            const ReferencedArguments& refArgs,
-                                                                            const CopiedArguments& copyArgs) const {
-        vrml_proc::core::logger::LogDebug(vrml_proc::core::utils::FormatString("Retrieve action by key <", key, ">."),
-                                          LOGGING_INFO);
+      std::shared_ptr<ConversionContextAction<ConversionContext>> GetAction(
+          const std::string& key, const ReferencedArguments& refArgs, const CopiedArguments& copyArgs) const {
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Retrieve action by key <", key, ">."), LOGGING_INFO);
         auto iterator = m_actions.find(key);
         if (iterator != m_actions.end()) {
           return iterator->second(refArgs, copyArgs);
@@ -86,8 +136,36 @@ namespace vrml_proc {
         return nullptr;
       }
 
+      std::shared_ptr<ConversionContextAction<ConversionContext>> GetAction(
+          const std::string& key, NodeViewArgument arg) const {
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Retrieve action by key <", key, ">."), LOGGING_INFO);
+        auto iterator = m_nodeViewActions.find(key);
+        if (iterator != m_nodeViewActions.end()) {
+          return iterator->second(arg);
+        }
+        vrml_proc::core::logger::LogWarning(
+            vrml_proc::core::utils::FormatString("Action with key <", key, "> was not found!"), LOGGING_INFO);
+        return nullptr;
+      }
+
+      std::shared_ptr<ConversionContextAction<ConversionContext>> GetAction(
+          const std::string& key, HandlerToActionBundleArgument arg) const {
+        vrml_proc::core::logger::LogDebug(
+            vrml_proc::core::utils::FormatString("Retrieve action by key <", key, ">."), LOGGING_INFO);
+        auto iterator = m_handlerToActionBundleActions.find(key);
+        if (iterator != m_handlerToActionBundleActions.end()) {
+          return iterator->second(arg);
+        }
+        vrml_proc::core::logger::LogWarning(
+            vrml_proc::core::utils::FormatString("Action with key <", key, "> was not found!"), LOGGING_INFO);
+        return nullptr;
+      }
+
      private:
       std::unordered_map<std::string, ActionFunctor> m_actions;
+      std::unordered_map<std::string, NodeViewActionFunctor> m_nodeViewActions;
+      std::unordered_map<std::string, HandlerToActionBundleActionFunctor> m_handlerToActionBundleActions;
     };
   }  // namespace action
 }  // namespace vrml_proc
