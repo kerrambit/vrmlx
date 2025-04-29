347ea72913de4fba321bd883b35276a24f35309d
Marek Eibel <536376@mail.muni.cz>
2025-04-29 18:45:30 +0200

    fix: mesh simplificator now takes UnitInterval

--- to_geom/src/calculators/MeshSimplificator.hpp
+++ to_geom/src/calculators/MeshSimplificator.hpp
@@ -5,35 +5,48 @@
 
 #include "FormatString.hpp"
 #include "Logger.hpp"
+#include "ManualTimer.hpp"
 #include "Mesh.hpp"
+#include "UnitInterval.hpp"
 
-#include "VrmlToGeomExport.hpp"
-
-namespace to_geom::calculator::MeshUtils {
-
-  inline VRMLTOGEOM_API void SimplifyMesh(to_geom::core::Mesh& mesh, double stopRation) {  //
+namespace to_geom::calculator::MeshSimplificator {
+  /**
+   * @brief Simplifies the mesh by reducing the number of edges.
+   *
+   * @param mesh mesh to simplify
+   * @param stopRatio number <0.0, 1.0> indicating when the simplification must finish, meaning that in the end 1.0 -
+   * `stopRatio` of the original mesh edges are collapsed
+   */
+  inline void SimplifyMesh(to_geom::core::Mesh& mesh, vrml_proc::core::utils::UnitInterval stopRatio) {  //
 
     using namespace vrml_proc::core::logger;
     using namespace vrml_proc::core::utils;
 
-    LogInfo(FormatString("Simplify the mesh (stop ratio is set to ", stopRation, " - stop simplifying when reaching ",
-                stopRation, " of the original number of edges)."),
+    LogInfo(
+        FormatString("Simplify the mesh (stop ratio is set to ", stopRatio.GetValue(), " - stopping simplifying when ",
+            100.0 - stopRatio.GetAsPercentage(), "% of the original number of edges are collapsed)."),
         LOGGING_INFO);
 
     if (!CGAL::is_triangle_mesh(mesh)) {
-      std::cerr << "Mesh is not a pure triangle mesh." << std::endl;
+      LogError("Mesh cannot be simplified, because the mesh is not a pure triangular mesh.", LOGGING_INFO);
       return;
     }
 
     if (!CGAL::is_valid_polygon_mesh(mesh)) {
-      std::cerr << "Mesh is not a valid polygon mesh." << std::endl;
+      LogError("Mesh cannot be simplified, because the mesh is not a valid polygon mesh.", LOGGING_INFO);
       return;
     }
 
+    ManualTimer timer;
+    timer.Start();
+
     namespace SMS = CGAL::Surface_mesh_simplification;
-    SMS::Count_ratio_stop_predicate<to_geom::core::Mesh> stop(stopRation);
+    SMS::Count_ratio_stop_predicate<to_geom::core::Mesh> stop(stopRatio.GetValue());
     int collapsedEdgesCount = SMS::edge_collapse(mesh, stop);
 
-    LogInfo(FormatString("Simplification finished. ", collapsedEdgesCount, " were collapsed."), LOGGING_INFO);
+    double time = timer.End();
+
+    LogInfo(FormatString("Simplification took ", time, " seconds. In total, ", collapsedEdgesCount, " were collapsed."),
+        LOGGING_INFO);
   }
-}  // namespace to_geom::calculator::MeshUtils
+}  // namespace to_geom::calculator::MeshSimplificator
