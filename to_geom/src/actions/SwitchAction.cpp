44ea71c46eef17fae46b14f73c2c356d2f2ccca6
Marek Eibel <536376@mail.muni.cz>
2025-05-08 17:49:17 +0200

    fix: simpify switch action

--- to_geom/src/actions/SwitchAction.cpp
+++ to_geom/src/actions/SwitchAction.cpp
@@ -11,11 +11,6 @@ namespace to_geom::action {
 
   std::shared_ptr<to_geom::conversion_context::MeshTaskConversionContext> SwitchAction::Execute() {
     vrml_proc::core::logger::LogDebug("Execute SwitchAction.", LOGGING_INFO);
-
-    auto result = std::make_shared<to_geom::conversion_context::MeshTaskConversionContext>();
-    result->Merge(m_properties.choice.get());
-    m_properties.choice.reset();
-
-    return result;
+    return m_properties.choice;
   }
 }  // namespace to_geom::action
